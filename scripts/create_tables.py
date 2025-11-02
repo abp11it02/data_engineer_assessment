cccba465f0860c0f31938f5170cd0834ff6d84af
Abhinav <abhinavprakash01@gmail.com>
2025-11-02 21:15:28 +0530

    Update create_tables.py

--- scripts/create_tables.py
+++ scripts/create_tables.py
@@ -1,15 +1,18 @@
 import mysql.connector
+import os
 
-conn = mysql.connector.connect(host = "127.0.0.1", port = 3306, user = "db_user" , password = "6equj5_db_user")
-cur  = conn.cursor()
+# conn = mysql.connector.connect(host = "127.0.0.1", port = 3306, user = "db_user" , password = "6equj5_db_user")
+# cur  = conn.cursor()
 
-cur.execute("show databases;")
-filepath = "../data/fake_property_data_new.json"
+# cur.execute("show databases;")
+script_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(script_dir)
+filepath = os.path.join(parent_dir, "sql", "create_table.sql")
 with open(filepath, 'r') as file:
     sql_script = file.read()
 statements = [stmt.strip() for stmt in sql_script.split(';')]
 print(statements)
-print(cur.fetchall())
+# print(cur.fetchall())
 
-cur.close()
-conn.close()
+# cur.close()
+# conn.close()
